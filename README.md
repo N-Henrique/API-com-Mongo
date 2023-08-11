1c20ac6fdac68ac8e11a74419183f16eac986fbe
Henrique <n.n.henrique.n.n@gmail.com>
2023-08-11 15:11:05 -0300

    rotas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # API-com-Mongo
+
+º Faça a aplicação se conectar com o banco de dados NoSQL.
+
+º Realizar operações no banco de dados.
--- src/index.js
+++ src/index.js
@@ -1,19 +1,88 @@
 const express = require("express");
 require("dotenv").config();
-const connectToDatabase = require("./db/mongo");
+const { ObjectId } = require("mongodb");
+const mongo = require("./db/mongo");
 
 const app = express();
 app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const dbObject = await connectToDatabase();
-    const usuariosCollection = dbObject.db.collection("usuarios");
-    const usuarios = await usuariosCollection.find({}).toArray();
-    return res.status(200).json(usuarios);
+    const conexao = await mongo();
+    const collection = conexao.db.collection("usuarios");
+    const usuario = await collection.find({}).toArray();
+    return res.status(200).json(usuario);
   } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error("Erro rota GET:", error);
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+});
+
+app.patch("/:id", async (req, res) => {
+  try {
+    const body = req.body;
+    const id = req.params.id;
+    const conexao = await mongo();
+    const collection = conexao.db.collection("usuarios");
+
+    const result = await collection.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          nome: body.nome,
+          anoDeNascimento: body.anoDeNascimento,
+          sobrenome: body.sobrenome,
+        },
+      },
+      { returnOriginal: false }
+    );
+
+    if (result.value) {
+      return res.status(200).json({
+        message: "Usuário atualizado com sucesso",
+        user: result.value,
+      });
+    } else {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
+  } catch (error) {
+    console.error("Erro rota PATCH:", error);
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const conexao = await mongo();
+    const collection = conexao.db.collection("usuarios");
+    const resultado = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    if (resultado.deletedCount === 1) {
+      return res.status(200).json({ message: "Usuário deletado com sucesso" });
+    } else {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
+  } catch (error) {
+    console.error("Erro rota DELETE:", error);
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+});
+
+app.post("/:id", async (req, res) => {
+  try {
+    const body = req.body;
+    const conexao = await mongo();
+    const collection = conexao.db.collection("usuarios");
+    const usuario = await collection.insertOne({
+      nome: body.nome,
+      sobrenome: body.sobrenome,
+      anoDeNascimento: body.anoDeNascimento,
+    });
+    return res.status(200).json(usuario);
+  } catch {
+    console.error("Erro rota POST:", error);
+    res.status(500).json({ error: "Erro no servidor" });
   }
 });
 
